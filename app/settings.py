6addcbd2485c6f772044e5340d99005832809270
J. Michael C <nospam5@doubleserver.com>
2020-05-16 22:01:34 -0700

    release deployment support

---
+++ app/settings.py
@@ -0,0 +1,13 @@
+
+
+# debug:
+k_http = 'http'
+k_ws = 'ws'
+k_url_prefix = ''
+k_static_url = 'http://localhost:8001/static/'
+
+# deploy:
+#k_http = 'https'
+#k_ws = 'wss'
+#k_url_prefix = '/practice'
+#k_static_url = 'https://openhome.school/practice/static/'
