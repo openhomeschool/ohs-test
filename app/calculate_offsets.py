d4c0fdc8a1581b16bf54ce6f2e110544284376b5
J. Michael C <nospam5@doubleserver.com>
2020-08-14 23:51:08 -0700

    more offset work

--- app/calculate_offsets.py
+++ app/calculate_offsets.py
@@ -1,50 +1,34 @@
 import sqlite3
 
 k_grammar_school_program = 1
+k_middle_school_program = 2
+k_7_9_program = 3
 k_tutor_role = 4
 
-def go():
+def go(program, reducer = None):
 	
 	db = sqlite3.connect('../ohs-test/ohs-test.db')
 	db.row_factory = sqlite3.Row
 
-	# Get total grammar tuitions:
-	c = db.execute(f'select amount from cost where program={k_grammar_school_program} and name="tuition"')
+	# Get total tuitions:
+	c = db.execute(f'select amount from cost where program={program} and name="tuition"')
 	tuition = c.fetchone()['amount']
-	c = db.execute(f'select count(*) from enrollment where program={k_grammar_school_program}')
+	c = db.execute(f'select count(*) from enrollment where program={program}')
 	enrollment = c.fetchone()['count(*)']
 	# And tutors:
-	c = db.execute(f'select count(*) from leader where program={k_grammar_school_program} and leadership_role={k_tutor_role}')
-	grammar_tutor_count = c.fetchone()['count(*)']
-	science_coordinator_offset = art_coordinator_offset = tuition * enrollment / enrollment**0.92
-	grammar_tutor_offset = int((tuition * enrollment - science_coordinator_offset - art_coordinator_offset) / grammar_tutor_count)
-	c = db.execute(f'update leader set annual_offset = {grammar_tutor_offset} where program={k_grammar_school_program} and leadership_role={k_tutor_role}')
+	c = db.execute(f'select sum(multiplier) as sum from leader where program={program} and leadership_role={k_tutor_role}')
+	tutor_count = c.fetchone()['sum']
+	reductions = 0
+	if reducer:
+		reductions = reducer(tuition, enrollment)
+	tutor_offset = int((tuition * enrollment - reductions) / tutor_count)
+	c = db.execute(f'update leader set annual_offset = multiplier * {tutor_offset} where program={program} and leadership_role={k_tutor_role}')
 	db.commit()
 
+def science_art_coordinators_reducer(tuition, enrollment):
+	return 2 * tuition * enrollment / enrollment**0.92 # 2* -- one for science-coordinator and one for art-coordinator
 
 if __name__ == "__main__":
-	go()
-
-
-
-
-	"""
-	c = db.execute(f'''
-			select * from leader
-			join leadership_role on leader.leadership_role = leadership_role.id
-			where leadership_role.name = "Tutor"
-			and program = {k_grammar_school_program}
-		''')
-	leaders = c.fetchall()
-	c = db.execute('''
-			select event.*, cycle_week.cycle, cycle_week.week, location.name as location, qr_key.key as qrcode_key, history_cycle_week.cycle as history_cycle, history_cycle_week.week as history_week, history_qr_key_table.key as history_qr_key from event
-			join location on region = location.id
-			join cycle_week on event.cw = cycle_week.id
-			join qr_key on event.qr_key = qr_key.id
-			left outer join history on event.id = history.event
-			left outer join cycle_week as history_cycle_week on history.cw = history_cycle_week.id
-			left outer join qr_key as history_qr_key_table on history.qr_key = history_qr_key_table.id
-			where event.subseq is null
-			and primary_sentence is not null order by cycle_week.cycle, cycle_week.week, event.seq
-		''') # and event.id=152     and event.id in (60, 58, 57, 56, 55, 53, 52, 51)
-	"""
+	#go(k_grammar_school_program, science_art_coordinators_reducer)
+	#go(k_middle_school_program)
+	go(k_7_9_program)
