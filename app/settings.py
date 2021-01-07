834f5869f3dd31029010c2d2673c4ab7b3b1ef02
openhomeschool <nospam6@doubleserver.com>
2021-01-07 15:23:17 -0500

    updated deployment values

--- app/settings.py
+++ app/settings.py
@@ -1,18 +1,19 @@
 
 
 # debug:
-k_http = 'http'
-k_ws = 'ws'
-k_url_prefix = ''
-k_ws_url_prefix = ''
-k_static_url = 'http://localhost:8001/static/'
+#k_http = 'http'
+#k_ws = 'ws'
+#k_url_prefix = ''
+#k_ws_url_prefix = ''
+#k_static_url = 'http://localhost:8001/static/'
 
 # deploy:
-#k_http = 'https'
-#k_ws = 'wss'
-#k_url_prefix = '/practice'
-#k_ws_url_prefix = '/practice_ws'
-#k_static_url = 'https://openhome.school/practice/static/'
+k_http = 'https'
+k_ws = 'wss'
+k_url_prefix = '/A'
+k_ws_url_prefix = '/a_ws'
+k_static_url = 'https://openhome.school/A/static/'
+
 
 # Quiz URLs:
 k_history_sequence = '/quiz/history/sequence'
