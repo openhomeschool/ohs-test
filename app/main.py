2c23c06c7ac8f398699d80e002862b66d91e1644
openhomeschool <nospam5@doubleserver.com>
2021-12-13 18:50:06 -0800

    bumped week

--- app/main.py
+++ app/main.py
@@ -381,7 +381,7 @@ async def timeline_event_detail(record, details, signs):
 async def science_detail(record, details, signs):
 	return hr(html.science_detail(record, details, signs))
 
-k_temp_this_week = 11
+k_temp_this_week = 12
 k_temp_this_cycle = 2
 
 _links = lambda request: (
