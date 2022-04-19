3021d2471393d81bbd9db23af6d74e5fc6575f28
openhomeschool <nospam5@doubleserver.com>
2022-04-19 07:59:10 -0700

    bumped week and rev

--- app/html.py
+++ app/html.py
@@ -16,7 +16,7 @@ from dominate.util import raw
 from . import valid
 from . import settings
 
-k_cache_version = '?v=e4'
+k_cache_version = '?v=e5'
 
 # Classes ---------------------------------------------------------------------
 
