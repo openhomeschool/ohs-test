94c26d79119db5fda40f16943e08d5546ff7d8d2
openhomeschool <nospam5@doubleserver.com>
2022-04-25 21:21:50 -0700

    bumped

--- app/main.py
+++ app/main.py
@@ -386,7 +386,7 @@ async def timeline_event_detail(record, details, signs):
 async def science_detail(record, details, signs):
 	return hr(html.science_detail(record, details, signs))
 
-k_temp_this_week = 25
+k_temp_this_week = 26
 k_temp_this_cycle = 2
 
 # cool characters: ⌂♩♪♫♬▲►▼◄→
