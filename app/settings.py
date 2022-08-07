8a1abddb2992e837854125583a16f71939a9f2b8
openhomeschool <nospam6@doubleserver.com>
2022-08-07 01:19:41 -0400

    release values

--- app/settings.py
+++ app/settings.py
@@ -1,20 +1,21 @@
 
 
 # debug:
-debug = True
-k_http = 'http'
-k_ws = 'ws'
-k_url_prefix = ''
-k_ws_url_prefix = ''
-k_static_url = 'http://localhost:8001/static/' # for use with adev
+#debug = True
+#k_http = 'http'
+#k_ws = 'ws'
+#k_url_prefix = ''
+#k_ws_url_prefix = ''
+#k_static_url = 'http://localhost:8001/static/' # for use with adev
 #k_static_url = 'http://localhost:8080/static/' # for use with stright python -m aiohttp.web
 
 # deploy:
-#k_http = 'https'
-#k_ws = 'wss'
-#k_url_prefix = '' # '/A'
-#k_ws_url_prefix = '/ws' # '/a_ws'
-#k_static_url = 'https://openhome.school' + k_url_prefix + '/static/'
+debug = False
+k_http = 'https'
+k_ws = 'wss'
+k_url_prefix = '' # '/A'
+k_ws_url_prefix = '/ws' # '/a_ws'
+k_static_url = 'https://openhome.school' + k_url_prefix + '/static/'
 
 
 # Quiz URLs:
