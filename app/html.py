00783fafc88b76e76067684b7222732851bc2882
openhomeschool <nospam5@doubleserver.com>
2022-11-02 07:22:00 -0700

    bump week

--- app/html.py
+++ app/html.py
@@ -23,7 +23,7 @@ from . import settings
 from . import text
 from . import util as U
 
-k_cache_version = '?v=g7'
+k_cache_version = '?v=g9'
 
 # Classes ---------------------------------------------------------------------
 
