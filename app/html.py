eb27fa9af858212f1db9ca5c1349bf972a8f97d8
openhomeschool <nospam5@doubleserver.com>
2023-02-20 21:30:08 -0800

    bumped cache-buster

--- app/html.py
+++ app/html.py
@@ -23,7 +23,7 @@ from . import settings
 from . import text
 from . import util as U
 
-k_cache_version = '?v=i4'
+k_cache_version = '?v=i5'
 
 # Classes ---------------------------------------------------------------------
 
