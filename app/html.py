bfd47f489f62a71fff4252ad6739f344067a7f5a
jmcaine <jmcaine@caines.us>
2023-09-25 09:31:06 -0700

    bump version

--- app/html.py
+++ app/html.py
@@ -24,7 +24,7 @@ from . import settings
 from . import text
 from . import util as U
 
-k_cache_version = '?v=j7'
+k_cache_version = '?v=j8'
 
 # Classes ---------------------------------------------------------------------
 
