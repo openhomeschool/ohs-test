7099867a23baaaf890a48ed86868cc1b456e6d84
jmcaine <jmcaine@caines.us>
2023-09-25 22:03:21 -0700

    bump version

--- app/main.py
+++ app/main.py
@@ -893,7 +893,7 @@ async def timeline_event_detail(record, details, signs, host):
 async def science_detail(record, details, signs, host):
 	return hr(html.science_detail(record, details, signs, host))
 
-k_temp_this_week = 1
+k_temp_this_week = 2
 k_temp_this_cycle = 1
 k_temp_this_academic_year = 4
 
