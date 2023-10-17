53bb03c81ba4407eb5442a9eac9357efaabaca40
jmcaine <jmcaine@caines.us>
2023-10-16 21:58:51 -0700

    bumped

--- app/html.py
+++ app/html.py
@@ -24,7 +24,7 @@ from . import settings
 from . import text
 from . import util as U
 
-k_cache_version = '?v=k2'
+k_cache_version = '?v=l2'
 
 # Classes ---------------------------------------------------------------------
 
